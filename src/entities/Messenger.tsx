abed13a3c2cc55e555efbea1ab8aa2d89083b069
Антон Стерхов <sterhovanton18@gmail.com>
2023-04-11 01:00:58 +0400

    messanger interface

--- src/entities/Messenger.tsx
+++ src/entities/Messenger.tsx
@@ -3,8 +3,47 @@ import React from 'react';
 const Messenger = () => {
     return (
         <div className='container'>
-            <div className="messenger">
-                
+            <div className="messenger flex align-start">
+                <div className="list-of-dialogs">
+                    <div className="dialog">
+                        <div className="companion-name">
+                            <h4>{"Игорь"}:</h4>
+                        </div>
+                        <div className="last-message">
+                            <p>{"Как дела?"}</p>
+                        </div>
+                    </div>
+                    <div className="dialog">
+                        <div className="companion-name">
+                            <h4>{"Игорь"}:</h4>
+                        </div>
+                        <div className="last-message">
+                            <p>{"Как дела?"}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="current-dialog flex align-center">
+                    <h2 className='current-companion'>Диалог с: {"Игорь"}</h2>
+                    <div className="messages-area flex align-center">
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                        <div className="message">fdgdfgfd</div>
+                    
+                    </div>
+                    <div className="write-message flex jcc align-center">
+                        <textarea className="inputlogin" />
+                        <button className="buttonLogin">Отправить сообщение</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
